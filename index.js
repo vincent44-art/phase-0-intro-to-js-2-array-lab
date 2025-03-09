9d1fd3fac2d3884e16dcc646752d18be8382632d
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-09 14:05:01 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+
+module.exports = {
+  cats,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+};
